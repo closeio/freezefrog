2fdd18f49d1b58df1789c1185e8a52d50004c36d
Thomas Steinacher <tom@eggdrop.ch>
2016-03-08 11:49:10 -0800

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+import datetime
+from freezefrog import FreezeTime
+import time
+import unittest
+
+class FreezeFrogTestCase(unittest.TestCase):
+    def test_freeze(self):
+        dt = datetime.datetime.utcnow()
+        self.assertTrue(dt > datetime.datetime(2016, 1, 1))
+
+        with FreezeTime(datetime.datetime(2014, 1, 1)):
+            time.sleep(0.001)
+            dt = datetime.datetime.utcnow()
+            self.assertEqual(dt, datetime.datetime(2014, 1, 1))
+            self.assertTrue(isinstance(dt, datetime.datetime))
+            self.assertEqual(time.time(), 1388563200)
+
+        dt = datetime.datetime.utcnow()
+        self.assertTrue(dt > datetime.datetime(2016, 1, 1))
+
+    def test_freeze_tick(self):
+        with FreezeTime(datetime.datetime(2014, 1, 1), tick=True):
+            time.sleep(0.001)
+            dt = datetime.datetime.utcnow()
+            self.assertTrue(datetime.datetime(2014, 1, 1) < dt < datetime.datetime(2014, 1, 1, 0, 0, 1))
+            self.assertTrue(1388563200 < time.time() < 1388563201)
