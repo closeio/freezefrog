48466eb3a74f7ae44f5ca8c0516f3174d2710c3e
João Sampaio <joao.sampaio@close.com>
2020-07-16 19:08:47 -0300

    Make FreezeFrog simulate system naive datetime and tz

--- freezefrog/__init__.py
+++ freezefrog/__init__.py
@@ -1,12 +1,14 @@
-import calendar
 import datetime
+import time
+
+import pytz
 
 try:
     from unittest.mock import patch
 except ImportError:
     from mock import patch
 
-__all__ = ['FreezeTime']
+__all__ = ["FreezeTime"]
 
 
 real_datetime = datetime.datetime
@@ -28,94 +30,79 @@ class FakeDateTimeMeta(type):
 
 class FakeDateTime(with_metaclass(FakeDateTimeMeta, real_datetime)):
     """
-    A datetime replacement that lets you set utcnow() using set_utcnow().
+    A `datetime` mock class.
 
-    The clock starts ticking after calling set_utcnow().
+    Set the start datetime and the system timezone with `start(dt, tz)`. The
+    clock starts ticking after calling `start`.
     """
-    tz_delta = None
+
+    dt = None
+    tz = None
+    _start = None
 
     @classmethod
-    def utcnow(cls, *args, **kwargs):
-        if not hasattr(cls, 'dt'):
-            raise NotImplementedError(
-                'use {}.set_utcnow(datetime) first'.format(cls.__name__)
-            )
-        return cls._utcnow()
-
-    @classmethod
-    def set_utcnow(cls, dt):
+    def start(cls, dt, tz, tick):
         cls.dt = dt
+        cls.tz = tz
+        cls._start = time.monotonic() if tick else None
 
     @classmethod
-    def set_tz_delta(cls, tz_delta):
-        cls.tz_delta = tz_delta
+    def time_since_start(cls):
+        if cls._start is None:
+            return datetime.timedelta(seconds=0)
+        return datetime.timedelta(seconds=time.monotonic() - cls._start)
 
     @classmethod
-    def _utcnow(cls):
-        if not hasattr(cls, '_start'):
-            cls._start = real_datetime.utcnow()
-        return (real_datetime.utcnow() - cls._start) + cls.dt
+    def now(cls, tz=None):
+        if tz is None:
+            return cls.dt + cls.time_since_start()
+        return tz.normalize(cls.tz.localize(cls.dt) + cls.time_since_start())
 
     @classmethod
-    def now(cls, *args, **kwargs):
-        if cls.tz_delta is not None:
-            return cls.utcnow() + cls.tz_delta
-        else:
-            raise Exception(
-                '{}.now() requires setting tz_delta'.format(cls.__name__)
-            )
+    def today(cls):
+        return cls.now()
 
-
-class FakeFixedDateTime(FakeDateTime):
     @classmethod
-    def _utcnow(cls):
-        return cls.dt
+    def utcnow(cls):
+        return cls.now(tz=pytz.utc)
 
 
 def fake_time():
-    now = datetime.datetime.utcnow()
-    ts = calendar.timegm(now.timetuple())
-    ts += now.microsecond / 1e6
-    return ts
+    return FakeDateTime.tz.localize(datetime.datetime.now()).timestamp()
 
 
 class FreezeTime(object):
     """
     A context manager that freezes the datetime to the given datetime object.
 
-    If tick=True is passed, the clock will start ticking, otherwise the clock
-    will remain at the given datetime.
+    It simulates that the system timezone is the passed timezone.
 
-    If `tz_delta` is passed, `datetime.datetime.now()` can be used with the
-    given UTC delta which is added to the frozen datetime.
+    If `tick=True` is passed, the clock will tick, otherwise the clock will
+    remain at the given datetime.
 
     Additional patch targets can be passed via `extra_patch_datetime` and
-    `extra_patch_time` to patch the datetime class or time function if it was
-    already imported in a different module. For example, if module `x` contains
-    `from datetime import datetime` (as opposed to `import datetime`), it needs
-    to be patched separately (`extra_patch_datetime=['x.datetime']`).
+    `extra_patch_time` to patch the `datetime` class or `time` function if it
+    was already imported in a different module. For example, if module `x`
+    contains `from datetime import datetime` (as opposed to `import datetime`),
+    it needs to be patched separately (`extra_patch_datetime=['x.datetime']`).
     """
-    def __init__(self, dt, tick=False, extra_patch_datetime=[],
-                 extra_patch_time=[], tz_delta=None):
-        datetime_targets = ['datetime.datetime'] + extra_patch_datetime
-        time_targets = ['time.time'] + extra_patch_time
 
-        if tick:
-            self.datetime_cls = FakeDateTime
-        else:
-            self.datetime_cls = FakeFixedDateTime
+    def __init__(
+        self, dt, tz, tick=False, extra_patch_datetime=(), extra_patch_time=()
+    ):
+        datetime_targets = ("datetime.datetime",) + tuple(extra_patch_datetime)
+        time_targets = ("time.time",) + tuple(extra_patch_time)
 
-        self.patches = (
-            [patch(target, self.datetime_cls) for target in datetime_targets] +
-            [patch(target, fake_time) for target in time_targets]
-        )
+        self.patches = [
+            patch(target, FakeDateTime) for target in datetime_targets
+        ] + [patch(target, fake_time) for target in time_targets]
 
-        self.datetime_cls.set_utcnow(dt)
-
-        self.tz_delta = tz_delta
+        self._dt = dt
+        self._tz = tz
+        self._tick = tick
 
     def __enter__(self):
-        self.datetime_cls.set_tz_delta(self.tz_delta)
+        FakeDateTime.start(self._dt, self._tz, tick=self._tick)
 
         for p in self.patches:
             p.__enter__()
@@ -123,5 +110,3 @@ class FreezeTime(object):
     def __exit__(self, *args):
         for p in reversed(self.patches):
             p.__exit__(*args)
-
-        self.datetime_cls.set_tz_delta(None)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,72 +1,112 @@
 import datetime
-from freezefrog import FreezeTime
 import time
 import unittest
 
+import pytz
+
+from freezefrog import FreezeTime
+
 PAST_DATETIME = datetime.datetime(2014, 1, 1)
-PAST_TIME = 1388534400
+PAST_TIME_UTC_TIMESTAMP = 1388534400
+PAST_TIME_NEW_YORK_TIMESTAMP = 1388552400
+TEN_SEC_DELTA = datetime.timedelta(seconds=10)
 
 
 class FreezeFrogTestCase(unittest.TestCase):
-    def test_freeze(self):
-        dt = datetime.datetime.utcnow()
+    def test_freeze_no_tick_utc(self):
+        utc_localized_past_datetime = pytz.UTC.localize(PAST_DATETIME)
+
+        dt = datetime.datetime.now()
         self.assertTrue(dt > datetime.datetime(2016, 1, 1))
 
-        with FreezeTime(PAST_DATETIME):
+        with FreezeTime(PAST_DATETIME, pytz.UTC):
+            self.assertTrue(
+                isinstance(datetime.datetime.now(), datetime.datetime)
+            )
+
             time.sleep(0.001)
-            dt = datetime.datetime.utcnow()
-            self.assertEqual(dt, PAST_DATETIME)
-            self.assertTrue(isinstance(dt, datetime.datetime))
-            self.assertEqual(time.time(), PAST_TIME)
+            self.assertEqual(datetime.datetime.now(), PAST_DATETIME)
+            self.assertEqual(datetime.datetime.today(), PAST_DATETIME)
+            self.assertEqual(
+                datetime.datetime.utcnow(), utc_localized_past_datetime
+            )
 
-        dt = datetime.datetime.utcnow()
+            self.assertEqual(
+                datetime.datetime.now(pytz.UTC),
+                pytz.UTC.localize(PAST_DATETIME),
+            )
+
+            self.assertEqual(time.time(), PAST_TIME_UTC_TIMESTAMP)
+
+        dt = datetime.datetime.now()
         self.assertTrue(dt > datetime.datetime(2016, 1, 1))
+
+    def test_freeze_tick_utc(self):
+        utc_localized_past_datetime = pytz.UTC.localize(PAST_DATETIME)
+
+        dt = datetime.datetime.now()
+        self.assertTrue(dt > datetime.datetime(2016, 1, 1))
+
+        with FreezeTime(PAST_DATETIME, pytz.UTC, tick=True):
+            self.assertTrue(
+                isinstance(datetime.datetime.now(), datetime.datetime)
+            )
+
+            time.sleep(0.001)
+            self.assertTrue(
+                PAST_DATETIME
+                < datetime.datetime.now()
+                < PAST_DATETIME + TEN_SEC_DELTA
+            )
+            self.assertTrue(
+                PAST_DATETIME
+                < datetime.datetime.today()
+                < PAST_DATETIME + TEN_SEC_DELTA
+            )
+            self.assertTrue(
+                utc_localized_past_datetime
+                < datetime.datetime.utcnow()
+                < utc_localized_past_datetime + TEN_SEC_DELTA
+            )
+
+            self.assertTrue(
+                PAST_TIME_UTC_TIMESTAMP
+                < time.time()
+                < PAST_TIME_UTC_TIMESTAMP + 1
+            )
+
+        dt = datetime.datetime.now()
+        self.assertTrue(dt > datetime.datetime(2016, 1, 1))
+
+    def test_freeze_new_york(self):
+        ny_localized_past_datetime = pytz.timezone(
+            "America/New_York"
+        ).localize(PAST_DATETIME)
+        with FreezeTime(
+            PAST_DATETIME, pytz.timezone("America/New_York"), tick=True
+        ):
+            self.assertTrue(
+                ny_localized_past_datetime
+                < datetime.datetime.utcnow()
+                < ny_localized_past_datetime + TEN_SEC_DELTA
+            )
 
     def test_freeze_extra(self):
         from . import module
 
         # Doesn't work since we've imported the sample module already.
-        with FreezeTime(PAST_DATETIME):
+        with FreezeTime(PAST_DATETIME, pytz.UTC):
             t, dt = module.get_info()
-            self.assertNotEqual(t, PAST_TIME)
+            self.assertNotEqual(t, PAST_TIME_UTC_TIMESTAMP)
             self.assertNotEqual(dt, PAST_DATETIME)
 
         # Works as expected.
-        with FreezeTime(PAST_DATETIME,
-                        extra_patch_time=['tests.module.time'],
-                        extra_patch_datetime=['tests.module.datetime']):
+        with FreezeTime(
+            PAST_DATETIME,
+            pytz.UTC,
+            extra_patch_time=["tests.module.time"],
+            extra_patch_datetime=["tests.module.datetime"],
+        ):
             t, dt = module.get_info()
-            self.assertEqual(t, PAST_TIME)
+            self.assertEqual(t, PAST_TIME_UTC_TIMESTAMP)
             self.assertEqual(dt, PAST_DATETIME)
-
-    def test_freeze_tick(self):
-        with FreezeTime(PAST_DATETIME, tick=True):
-            time.sleep(0.001)
-            dt = datetime.datetime.utcnow()
-            start = PAST_DATETIME
-            end = PAST_DATETIME + datetime.timedelta(seconds=1)
-            self.assertTrue(start < dt < end)
-            self.assertTrue(PAST_TIME < time.time() < PAST_TIME+1)
-
-    def test_now(self):
-        regular_now = datetime.datetime.now()
-        self.assertTrue(regular_now)
-
-        with FreezeTime(PAST_DATETIME):
-            self.assertRaises(Exception, datetime.datetime.now)
-
-        tz_delta = datetime.timedelta()
-
-        with FreezeTime(PAST_DATETIME, tz_delta=tz_delta):
-            dt = datetime.datetime.now()
-            self.assertEqual(dt, PAST_DATETIME)
-
-        tz_delta = datetime.timedelta(hours=5)
-
-        with FreezeTime(PAST_DATETIME, tz_delta=tz_delta):
-            dt = datetime.datetime.now()
-            self.assertEqual(dt, PAST_DATETIME + tz_delta)
-
-        # check no stale tz_deltas remained
-        with FreezeTime(PAST_DATETIME):
-            self.assertRaises(Exception, datetime.datetime.now)
